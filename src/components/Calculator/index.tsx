b2b8a7db68e8b51e2073bbbbc5151a76d7023335
Juliette Rapala <juliette@eventric.com>
2020-06-27 15:05:40 -0500

    Add button handlers to number buttons and update current value

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 import { Text } from "react-native"
 import styled from "styled-components/native"
 
@@ -7,9 +7,29 @@ import CalculatorButton from "../CalculatorButton"
 import normalize from "../../utils/normalize"
 
 const Calculator: React.FC = () => {
+	const [currentValue, setCurrentValue] = useState<string>("0")
+
+	const handleTap = (type: string, value: number | string): void => {
+		setCurrentValue(currentState => {
+			if (type === "number") {
+				if (currentState === "0") {
+					return `${value}`
+				} else if (currentState.length <= 8) {
+					return `${currentState}${value}`
+				} else {
+					return `${currentState}`
+				}
+			} else {
+				return "0"
+			}
+		})
+	}
+
 	return (
 		<Fragment>
-			<CalculatorTotal>75</CalculatorTotal>
+			<CalculatorTotal>
+				{parseFloat(currentValue).toLocaleString()}
+			</CalculatorTotal>
 			<Row>
 				<CalculatorButton
 					text="AC"
@@ -34,9 +54,18 @@ const Calculator: React.FC = () => {
 				/>
 			</Row>
 			<Row>
-				<CalculatorButton text="7" onPress={() => alert("TODO")} />
-				<CalculatorButton text="8" onPress={() => alert("TODO")} />
-				<CalculatorButton text="9" onPress={() => alert("TODO")} />
+				<CalculatorButton
+					text="7"
+					onPress={(): void => handleTap("number", 7)}
+				/>
+				<CalculatorButton
+					text="8"
+					onPress={(): void => handleTap("number", 8)}
+				/>
+				<CalculatorButton
+					text="9"
+					onPress={(): void => handleTap("number", 9)}
+				/>
 				{/* Multiplication */}
 				<CalculatorButton
 					text="&#215;"
@@ -45,9 +74,18 @@ const Calculator: React.FC = () => {
 				/>
 			</Row>
 			<Row>
-				<CalculatorButton text="4" onPress={() => alert("TODO")} />
-				<CalculatorButton text="5" onPress={() => alert("TODO")} />
-				<CalculatorButton text="6" onPress={() => alert("TODO")} />
+				<CalculatorButton
+					text="4"
+					onPress={(): void => handleTap("number", 4)}
+				/>
+				<CalculatorButton
+					text="5"
+					onPress={(): void => handleTap("number", 5)}
+				/>
+				<CalculatorButton
+					text="6"
+					onPress={(): void => handleTap("number", 6)}
+				/>
 				{/* Subtraction */}
 				<CalculatorButton
 					text="&#8211;"
@@ -56,9 +94,18 @@ const Calculator: React.FC = () => {
 				/>
 			</Row>
 			<Row>
-				<CalculatorButton text="1" onPress={() => alert("TODO")} />
-				<CalculatorButton text="2" onPress={() => alert("TODO")} />
-				<CalculatorButton text="3" onPress={() => alert("TODO")} />
+				<CalculatorButton
+					text="1"
+					onPress={(): void => handleTap("number", 1)}
+				/>
+				<CalculatorButton
+					text="2"
+					onPress={(): void => handleTap("number", 2)}
+				/>
+				<CalculatorButton
+					text="3"
+					onPress={(): void => handleTap("number", 3)}
+				/>
 				<CalculatorButton
 					text="+"
 					onPress={() => alert("TODO")}
@@ -68,10 +115,13 @@ const Calculator: React.FC = () => {
 			<Row>
 				<CalculatorButton
 					text="0"
-					onPress={() => alert("TODO")}
+					onPress={(): void => handleTap("number", 0)}
 					size="double"
 				/>
-				<CalculatorButton text="." onPress={() => alert("TODO")} />
+				<CalculatorButton
+					text="."
+					onPress={(): void => handleTap("number", ".")}
+				/>
 				<CalculatorButton
 					text="="
 					onPress={() => alert("TODO")}
@@ -84,7 +134,9 @@ const Calculator: React.FC = () => {
 
 const CalculatorTotal = styled(Text)`
 	color: ${(props): string => props.theme.textColorOnBackgroundColor};
-	font-size: ${normalize(30) + "px"};
+	font-size: ${normalize(70) + "px"};
+	font-weight: 300;
+	margin-bottom: 2%;
 	margin-right: 5%;
 	text-align: right;
 `
